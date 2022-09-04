d4cd6303c55ce2d865c71469d82efd03c99fbe99
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2022-09-04 02:58:20 +0200

    feat: rm useless check validation on edit action

--- src/containers/Users/UserEdit/UserEdit.tsx
+++ src/containers/Users/UserEdit/UserEdit.tsx
@@ -3,10 +3,14 @@ import {
   PLACEHOLDER_FIRST_NAME,
   PLACEHOLDER_LAST_NAME,
   PLACEHOLDER_EMAIL,
+  PLACEHOLDER_PASSWORD,
+  PLACEHOLDER_NEW_PASSWORD,
   INPUT_NAME,
   LABEL_FIRST_NAME,
   LABEL_LAST_NAME,
   LABEL_EMAIL,
+  LABEL_PASSWORD,
+  LABEL_NEW_PASSWORD,
 } from './constants';
 
 function UserEdit({ onSubmit, initialValues }: any): any {
@@ -20,7 +24,7 @@ function UserEdit({ onSubmit, initialValues }: any): any {
   const handleSubmit = (values: object) => onSubmit(values);
 
   const renderForm = ({
-                        setFieldValue, values, errors, touched,
+                        setFieldValue, values,
                       }: any): any =>
     <Form className="mt-5">
       <div className="form-floating mb-3">
@@ -33,9 +37,6 @@ function UserEdit({ onSubmit, initialValues }: any): any {
           placeholder={PLACEHOLDER_FIRST_NAME}
           value={values[INPUT_NAME.FIRST_NAME]}
         />
-        {touched[INPUT_NAME.FIRST_NAME] && errors && errors[INPUT_NAME.FIRST_NAME] ? (
-          <span className="error-text">{errors[INPUT_NAME.FIRST_NAME]}</span>
-        ) : null}
         <label htmlFor="floatingFirstname">{LABEL_FIRST_NAME}</label>
       </div>
       <div className="form-floating mb-3">
@@ -48,11 +49,6 @@ function UserEdit({ onSubmit, initialValues }: any): any {
           placeholder={PLACEHOLDER_LAST_NAME}
           value={values[INPUT_NAME.LAST_NAME]}
         />
-        {touched[INPUT_NAME.LAST_NAME]
-        && errors
-        && errors[INPUT_NAME.LAST_NAME] ? (
-          <span className="error-text">{errors[INPUT_NAME.LAST_NAME]}</span>
-        ) : null}
         <label htmlFor="floatingLastname">{LABEL_LAST_NAME}</label>
       </div>
       <div className="form-floating mb-3">
@@ -65,12 +61,31 @@ function UserEdit({ onSubmit, initialValues }: any): any {
           placeholder={PLACEHOLDER_EMAIL}
           value={values[INPUT_NAME.EMAIL]}
         />
-        {touched[INPUT_NAME.EMAIL]
-        && errors
-        && errors[INPUT_NAME.EMAIL] ? (
-          <span className="error-text">{errors[INPUT_NAME.EMAIL]}</span>
-        ) : null}
         <label htmlFor="floatingEmail">{LABEL_EMAIL}</label>
+      </div>
+      <div className="form-floating mb-3">
+        <Field
+          id="floatingPassword"
+          className="form-control mb-2"
+          name={INPUT_NAME.PASSWORD}
+          type="password"
+          onChange={onChange(setFieldValue, INPUT_NAME.PASSWORD)}
+          placeholder={PLACEHOLDER_PASSWORD}
+          value={values[INPUT_NAME.PASSWORD]}
+        />
+        <label htmlFor="floatingEmail">{LABEL_PASSWORD}</label>
+      </div>
+      <div className="form-floating mb-3">
+        <Field
+          id="floatingNewPassword"
+          className="form-control mb-2"
+          name={INPUT_NAME.NEW_PASSWORD}
+          type="password"
+          onChange={onChange(setFieldValue, INPUT_NAME.NEW_PASSWORD)}
+          placeholder={PLACEHOLDER_NEW_PASSWORD}
+          value={values[INPUT_NAME.NEW_PASSWORD]}
+        />
+        <label htmlFor="floatingNewPassword">{LABEL_NEW_PASSWORD}</label>
       </div>
       <button className="btn btn-primary" type="submit">
         Save
