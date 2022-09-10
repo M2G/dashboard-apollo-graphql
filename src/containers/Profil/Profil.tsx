17547743084cdd4f305cc5be6b1a8cb13fd45eed
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2022-09-10 05:20:56 +0200

    fix: uncontrolled behaviour

--- src/containers/Profil/Profil.tsx
+++ src/containers/Profil/Profil.tsx
@@ -1,25 +1,36 @@
 /*eslint-disable*/
 import { useCallback } from 'react';
-//import { useLazyQuery } from "@apollo/client";
-import { INITIAL_VALUES } from './constants';
-//import { USER } from 'gql/queries/users';
+import { useLazyQuery } from "@apollo/client";
+import { USER } from 'gql/queries/users';
 import ProfilForm from 'components/ProfilForm';
+import { INPUT_NAME, INITIAL_VALUES }  from 'components/ProfilForm/constants';
+
+function initialValues() {
+  const initialValues = { ...INITIAL_VALUES };
+
+  initialValues[INPUT_NAME.FIRST_NAME] = '';
+  initialValues[INPUT_NAME.LAST_NAME] = '';
+  initialValues[INPUT_NAME.EMAIL] = '';
+  initialValues[INPUT_NAME.PASSWORD] = '';
+
+  return initialValues;
+}
 
 function Profil() {
-  //const [user] = useLazyQuery(USER,  { fetchPolicy: 'no-cache' });
+  const [user] = useLazyQuery(USER,  { fetchPolicy: 'no-cache' });
 
   const handleSubmit = useCallback(async (formData: any) => {
     console.log('formData', formData)
-    /*await user(
+    await user(
       {
         variables: {
           ...formData
         }
       }
-    );*/
+    );
   }, []);
 
-  return <ProfilForm initialValues={INITIAL_VALUES} onSubmit={handleSubmit} />;
+  return <ProfilForm initialValues={initialValues()} onSubmit={handleSubmit} />;
 }
 
 export default Profil;
