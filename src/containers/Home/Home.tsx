2b284b66c8301ca950430c2db627aeeda6c9d818
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-02-28 01:53:18 +0100

    fix: sentry and add header in page home

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -4,6 +4,9 @@ function Home() {
   return (
     <div className="o-zone">
       <div className="o-grid">
+        <header className="o-zone__head">
+          <h2 className="o-zone__head__title">test</h2>
+        </header>
         <div className="o-grid__row">
           <div className="o-col">
             <div className="o-cell--one">
