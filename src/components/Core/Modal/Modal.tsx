d10028a29ca7cd617922769e50a76481d1ea3f7b
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-05-10 04:37:18 +0200

    feat: update design modal

---
+++ src/components/Core/Modal/Modal.tsx
@@ -0,0 +1,70 @@
+import type { MouseEventHandler } from 'react';
+import clsx from 'clsx';
+import Portal from 'components/Core/Portal';
+import styles from './Modal.module.scss';
+
+interface IModal {
+  id?: string | undefined;
+  isShowing: boolean;
+  hide: MouseEventHandler<HTMLButtonElement>;
+  onConfirm: any;
+  title: string;
+  children: any;
+}
+
+function Modal({
+                 id,
+                 isShowing,
+                 hide,
+                 title,
+                 onConfirm,
+                 children,
+               }: IModal): JSX.Element | null {
+  return isShowing ? (
+    <Portal id={id}>
+      <div className={styles.overlay}>
+        <div className={styles.wrapper}>
+          <div className={clsx(styles.modal, 'modal-content rounded-4 shadow')}>
+            <div className={clsx(styles.header, 'border-bottom-0')}>
+              <h5 className={styles.title}>{title}</h5>
+              <button
+                type="button"
+                className={styles.close}
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                onClick={hide}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                     fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                     strokeLinejoin="round" className="w-4 h-4">
+                  <line x1="18" x2="6" y1="6" y2="18" />
+                  <line x1="6" x2="18" y1="6" y2="18" />
+                </svg>
+              </button>
+            </div>
+            <div className={styles.body}>{children}</div>
+            <div className="modal-footer border-top-0">
+              <button
+                type="button"
+                className="btn btn-light me-2"
+                onClick={onConfirm}
+              >
+                Confirmer
+              </button>
+              <button
+                type="button"
+                className="btn btn-light"
+                data-bs-dismiss="modal"
+                onClick={hide}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Portal>
+  ) : null;
+}
+
+export default Modal;
--- src/components/Core/Modal/ModalWrapper.tsx
+++ src/components/Core/Modal/ModalWrapper.tsx
@@ -1,16 +1,4 @@
-import type { MouseEventHandler } from 'react';
-import clsx from 'clsx';
-import Portal from 'components/Core/Portal';
-import styles from './Modal.module.scss';
-
-interface IModal {
-  id?: string | undefined;
-  isShowing: boolean;
-  hide: MouseEventHandler<HTMLButtonElement>;
-  onConfirm: any;
-  title: string;
-  children: any;
-}
+import Modal from './Modal';
 
 interface IModalWrapper {
   id?: string | undefined;
@@ -19,54 +7,6 @@ interface IModalWrapper {
   title: any;
   onConfirm: any;
   children: any;
-}
-
-function Modal({
-  id,
-  isShowing,
-  hide,
-  title,
-  onConfirm,
-  children,
-}: IModal): JSX.Element | null {
-  return isShowing ? (
-    <Portal id={id}>
-      <div className={styles.overlay}>
-        <div className={styles.wrapper}>
-          <div className={clsx(styles.modal, 'modal-content rounded-4 shadow')}>
-            <div className={clsx(styles.header, 'border-bottom-0')}>
-              <h5 className="modal-title">{title}</h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-                onClick={hide}
-              />
-            </div>
-            <div className="modal-body py-0">{children}</div>
-            <div className="modal-footer flex-column border-top-0">
-              <button
-                type="button"
-                className="btn btn-lg btn-primary w-100 mx-0 mb-2"
-                onClick={onConfirm}
-              >
-                Confirmer
-              </button>
-              <button
-                type="button"
-                className="btn btn-lg btn-light w-100 mx-0"
-                data-bs-dismiss="modal"
-                onClick={hide}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Portal>
-  ) : null;
 }
 
 function ModalWrapper({
