3a2671204e3150836170f0d7ffb89fdff3709386
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-11 21:30:50 +0100

    feat: update constants

--- apps/web/src/components/ResetPasswordForm/constants.ts
+++ apps/web/src/components/ResetPasswordForm/constants.ts
@@ -25,13 +25,16 @@ export const PLACEHOLDER_VERIFY_PASSWORD = 'Verify password';
 export const formSchema = z.object({
   [INPUT_NAME.NEW_PASSWORD]: z
     .string()
-    .min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_VERIFY_PASSWORD),
+    .min(8, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_VERIFY_PASSWORD),
   [INPUT_NAME.VERIFY_PASSWORD]: z
     .string()
-    .min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_VERIFY_PASSWORD)
-    .refine((data) => data[INPUT_NAME.NEW_PASSWORD] === data[INPUT_NAME.VERIFY_PASSWORD], {
-      path: [INPUT_NAME.VERIFY_PASSWORD],
-      message: "Password don't match",
-    }),
-  // .min(8, 'Password must have more than 8 characters'),
+    .min(8, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_VERIFY_PASSWORD)
+    .refine(
+      (data) =>
+        data[INPUT_NAME.NEW_PASSWORD] === data[INPUT_NAME.VERIFY_PASSWORD],
+      {
+        path: [INPUT_NAME.VERIFY_PASSWORD],
+        message: "Password don't match",
+      },
+    ),
 });
