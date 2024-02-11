1ee70ecca857010f048301f34ff865bde1e2f8b3
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-11 21:28:38 +0100

    feat: add test profil form

--- apps/web/src/components/ProfilForm/ProfilForm.test.tsx
+++ apps/web/src/components/ProfilForm/ProfilForm.test.tsx
@@ -1,7 +1,14 @@
 /*eslint-disable*/
-import { fireEvent, render, cleanup } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  cleanup,
+  screen,
+  act,
+} from '@testing-library/react';
 import ProfilForm from './ProfilForm';
-import { INPUT_NAME } from './constants';
+import { INITIAL_VALUES } from './constants';
+import { MemoryRouter } from 'react-router-dom';
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: () => ['key'] }),
@@ -10,42 +17,60 @@ jest.mock('react-i18next', () => ({
 
 afterEach(cleanup);
 
-describe('Signin Container', () => {
+describe('Profil Form Component', () => {
   describe('Submitting form', () => {
     let wrapper: any;
-    let floatingInput: HTMLInputElement;
-    let floatingPassword: HTMLInputElement;
+    let floatingEmail: HTMLInputElement;
+    let floatingFirstName: HTMLInputElement;
+    let floatingLastName: HTMLInputElement;
+    let floatingSubmit: HTMLInputElement;
     const onSubmit = jest.fn();
 
-    beforeEach(() => {
-      const INITIAL_VALUES = {
-        [INPUT_NAME.EMAIL]: '',
-        [INPUT_NAME.PASSWORD]: '',
-      };
-
-      wrapper = render(
-        <ProfilForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />,
+    beforeEach(async () => {
+      render(
+        <MemoryRouter>
+          <ProfilForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />
+        </MemoryRouter>,
       );
 
-      floatingInput = wrapper.container.querySelector('#floatingInput');
-      floatingPassword = wrapper.container.querySelector('#floatingPassword');
+      floatingEmail = screen.getByTestId('email');
+      floatingFirstName = screen.getByTestId('first_name');
+      floatingLastName = screen.getByTestId('last_name');
+      floatingSubmit = screen.getByTestId('submit');
 
-      fireEvent.change(floatingInput, { target: { value: 'test@gmail.com' } });
-      fireEvent.change(floatingPassword, { target: { value: 'test' } });
+      fireEvent.change(floatingEmail, { target: { value: 'test' } });
+      fireEvent.change(floatingFirstName, { target: { value: 'test' } });
+      fireEvent.change(floatingLastName, { target: { value: 'test' } });
 
-      expect(floatingInput).toBeInTheDocument();
-      expect(floatingPassword).toBeInTheDocument();
+      await act(() => {
+        fireEvent.submit(floatingSubmit);
+      });
+    });
 
-      const button: any = wrapper.container.querySelector('.btn');
-      fireEvent.click(button);
+    test('should display error validatiob', async () => {
+      fireEvent.change(floatingEmail, { target: { value: '' } });
+      fireEvent.change(floatingFirstName, { target: { value: '' } });
+      fireEvent.change(floatingLastName, { target: { value: '' } });
+
+      await act(() => {
+        fireEvent.submit(floatingSubmit);
+      });
     });
 
     test('should render', () => {
       expect(onSubmit).toHaveBeenCalledTimes(1);
-      expect(onSubmit).toHaveBeenCalledWith({
-        email: 'test@gmail.com',
-        password: 'test',
+      expect(onSubmit.mock.calls[0][0]).toMatchObject({
+        email: 'test',
+        first_name: 'test',
+        last_name: 'test',
       });
+      /*
+      expect(onSubmit).toHaveBeenCalledWith({
+        email: 'test',
+        first_name: 'test',
+        last_name: 'test',
+      });
+      */
     });
   });
 });
