130f1b09777fd009e83a40964d95d514f438abc7
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-11 21:25:58 +0100

    feat: add test for change pwd form

--- apps/web/src/components/ChangePassordForm/ChangePassordForm.test.tsx
+++ apps/web/src/components/ChangePassordForm/ChangePassordForm.test.tsx
@@ -6,7 +6,7 @@ import {
   screen,
   act,
 } from '@testing-library/react';
-import ForgotPasswordForm from './ForgotPasswordForm';
+import ChangePassordForm from './ChangePassordForm';
 import { INPUT_NAME, INITIAL_VALUES } from './constants';
 import { MemoryRouter } from 'react-router-dom';
 
@@ -20,33 +20,41 @@ afterEach(cleanup);
 
 describe('Reset Password Form Component', () => {
   describe('Submitting form', () => {
-    let floatingEmail: HTMLInputElement;
-    let floatingSubmit: HTMLInputElement;
+    let oldPassword: HTMLInputElement;
+    let password: HTMLInputElement;
+    let newPassword: HTMLInputElement;
+    let submit: HTMLInputElement;
     const onSubmit = jest.fn();
 
     beforeEach(async () => {
       //@see https://stackoverflow.com/questions/76081552/typeerror-cannot-destructure-property-basename-of-react-namespace-usecontex
       render(
         <MemoryRouter>
-          <ForgotPasswordForm
+          <ChangePassordForm
             initialValues={INITIAL_VALUES}
             onSubmit={onSubmit}
           />
         </MemoryRouter>,
       );
 
-      floatingEmail = screen.getByTestId('email');
-      floatingSubmit = screen.getByTestId('submit');
+      oldPassword = screen.getByTestId('oldPassword');
+      password = screen.getByTestId('password');
+      newPassword = screen.getByTestId('password');
+      submit = screen.getByTestId('submit');
 
-      fireEvent.change(floatingEmail, { target: { value: 'test' } });
+      fireEvent.change(oldPassword, { target: { value: 'test' } });
+      fireEvent.change(password, { target: { value: 'test2' } });
+      fireEvent.change(newPassword, { target: { value: 'test2' } });
 
       await act(() => {
-        fireEvent.submit(floatingSubmit);
+        fireEvent.submit(submit);
       });
     });
 
     test('should display correctly value form input', () => {
-      expect(floatingEmail.value).toBe('test');
+      expect(oldPassword.value).toBe('test');
+      expect(password.value).toBe('test2');
+      expect(newPassword.value).toBe('test2');
     });
 
     test('should submit data', () => {
