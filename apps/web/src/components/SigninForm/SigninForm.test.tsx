1e848c133b3d1f459bb370e5a52892fccab43760
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-11 21:31:36 +0100

    feat: add test signin form

--- apps/web/src/components/SigninForm/SigninForm.test.tsx
+++ apps/web/src/components/SigninForm/SigninForm.test.tsx
@@ -1,7 +1,13 @@
 /*eslint-disable*/
-import { fireEvent, render, cleanup, screen } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  cleanup,
+  screen,
+  act,
+} from '@testing-library/react';
 import SigninForm from './SigninForm';
-import { INPUT_NAME } from './constants';
+import { INPUT_NAME, INITIAL_VALUES } from './constants';
 import { MemoryRouter } from 'react-router-dom';
 
 jest.mock('react-i18next', () => ({
@@ -12,19 +18,15 @@ jest.mock('react-i18next', () => ({
 
 afterEach(cleanup);
 
-describe('Signin Container', () => {
+describe('Signin Form Component', () => {
   describe('Submitting form', () => {
     let wrapper: any;
     let floatingInput: HTMLInputElement;
     let floatingPassword: HTMLInputElement;
+    let floatingSubmit: HTMLInputElement;
     const onSubmit = jest.fn();
 
-    beforeEach(() => {
-      const INITIAL_VALUES = {
-        [INPUT_NAME.EMAIL]: '',
-        [INPUT_NAME.PASSWORD]: '',
-      };
-
+    beforeEach(async () => {
       //@see https://stackoverflow.com/questions/76081552/typeerror-cannot-destructure-property-basename-of-react-namespace-usecontex
       render(
         <MemoryRouter>
@@ -32,33 +34,50 @@ describe('Signin Container', () => {
         </MemoryRouter>,
       );
 
-      const tt = screen.getByTestId('email');
-
-      console.log('tt', tt);
-
-      /*
-
-      floatingInput = wrapper.container.querySelector('#floatingInput');
-      floatingPassword = wrapper.container.querySelector('#floatingPassword');
+      floatingInput = screen.getByTestId('email');
+      floatingPassword = screen.getByTestId('password');
+      floatingSubmit = screen.getByTestId('submit');
 
       fireEvent.change(floatingInput, { target: { value: 'test@gmail.com' } });
       fireEvent.change(floatingPassword, { target: { value: 'test' } });
 
-      expect(floatingInput).toBeInTheDocument();
-      expect(floatingPassword).toBeInTheDocument();
-
-      const button: any = wrapper.container.querySelector('.btn');
-      fireEvent.click(button);
+      await act(() => {
+        fireEvent.submit(floatingSubmit);
+      });
     });
 
-    test('should render', () => {
+    test('should display error validatiob', async () => {
+      fireEvent.change(floatingInput, { target: { value: '' } });
+      fireEvent.change(floatingPassword, { target: { value: '' } });
+
+      await act(() => {
+        fireEvent.submit(floatingSubmit);
+      });
+
+      expect(screen.getByText('Invalid email')).toBeInTheDocument();
+      expect(screen.getByText('Password is required')).toBeInTheDocument();
+    });
+
+    test('should display correctly value form input', () => {
+      expect(floatingInput.value).toBe('test@gmail.com');
+      expect(floatingPassword.value).toBe('test');
+    });
+
+    test('should submit data', () => {
       expect(onSubmit).toHaveBeenCalledTimes(1);
-      expect(onSubmit).toHaveBeenCalledWith({
+      expect(onSubmit.mock.calls[0][0]).toMatchObject({
         email: 'test@gmail.com',
         password: 'test',
-      });*/
-    });
+      });
 
-    test('should render', () => {});
+      /*
+      expect(onSubmit).toHaveBeenCalledWith(
+        {
+          email: 'test@gmail.com',
+          password: 'test',
+        }
+      );
+      */
+    });
   });
 });
