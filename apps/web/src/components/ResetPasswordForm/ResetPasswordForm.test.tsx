8c2d6f629349315d76d02915ff41aa90f4b282a8
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-11 21:29:24 +0100

    feat: add tests reset pwd form

--- apps/web/src/components/ResetPasswordForm/ResetPasswordForm.test.tsx
+++ apps/web/src/components/ResetPasswordForm/ResetPasswordForm.test.tsx
@@ -1,46 +1,90 @@
 /*eslint-disable*/
-import { fireEvent, render, cleanup } from '@testing-library/react';
-import SignupForm from './SignupForm';
-import { INPUT_NAME } from './constants';
+import {
+  fireEvent,
+  render,
+  cleanup,
+  screen,
+  act,
+} from '@testing-library/react';
+import ResetPasswordForm from './ResetPasswordForm';
+import { INPUT_NAME, INITIAL_VALUES } from './constants';
+import { MemoryRouter } from 'react-router-dom';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({ t: () => ['key'] }),
+  Trans: () => jest.fn(),
+  t: () => jest.fn(),
+}));
 
 afterEach(cleanup);
 
-describe('Signin Container', () => {
+describe('Reset Password Form Component', () => {
   describe('Submitting form', () => {
     let wrapper: any;
-    let floatingInput: HTMLInputElement;
-    let floatingPassword: HTMLInputElement;
+    let floatingNewPassword: HTMLInputElement;
+    let floatingVerifyPassword: HTMLInputElement;
+    let floatingSubmit: HTMLInputElement;
     const onSubmit = jest.fn();
 
-    beforeEach(() => {
-      const INITIAL_VALUES = {
-        [INPUT_NAME.EMAIL]: '',
-        [INPUT_NAME.PASSWORD]: ''
-      };
-
-      wrapper = render(
-        <SignupForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />
+    beforeEach(async () => {
+      //@see https://stackoverflow.com/questions/76081552/typeerror-cannot-destructure-property-basename-of-react-namespace-usecontex
+      render(
+        <MemoryRouter>
+          <ResetPasswordForm
+            initialValues={INITIAL_VALUES}
+            onSubmit={onSubmit}
+          />
+        </MemoryRouter>,
       );
 
-      floatingInput = wrapper.container.querySelector('#floatingInput');
-      floatingPassword = wrapper.container.querySelector('#floatingPassword');
+      floatingNewPassword = screen.getByTestId('new_password');
+      floatingVerifyPassword = screen.getByTestId('verify_password');
+      floatingSubmit = screen.getByTestId('submit');
 
-      fireEvent.change(floatingInput, { target: { value: 'test@gmail.com' } });
-      fireEvent.change(floatingPassword, { target: { value: 'test' } });
+      fireEvent.change(floatingNewPassword, { target: { value: 'test' } });
+      fireEvent.change(floatingVerifyPassword, { target: { value: 'test' } });
 
-      expect(floatingInput).toBeInTheDocument();
-      expect(floatingPassword).toBeInTheDocument();
-
-      const button: any = wrapper.container.querySelector('.btn');
-      fireEvent.click(button);
+      await act(() => {
+        fireEvent.submit(floatingSubmit);
+      });
     });
 
-    test('should render', () => {
-      expect(onSubmit).toHaveBeenCalledTimes(1);
-      expect(onSubmit).toHaveBeenCalledWith({
-        email: 'test@gmail.com',
-        password: 'test'
+    test('should display error validatiob', async () => {
+      fireEvent.change(floatingNewPassword, { target: { value: '' } });
+      fireEvent.change(floatingVerifyPassword, { target: { value: '' } });
+
+      await act(() => {
+        fireEvent.submit(floatingSubmit);
       });
+
+      expect(
+        screen.getAllByText('Verify password requis')[0],
+      ).toBeInTheDocument();
+      expect(
+        screen.getAllByText('Verify password requis')[1],
+      ).toBeInTheDocument();
+    });
+
+    test('should display correctly value form input', () => {
+      expect(floatingVerifyPassword.value).toBe('test');
+      expect(floatingVerifyPassword.value).toBe('test');
+    });
+
+    test('should submit data', () => {
+      expect(onSubmit).toHaveBeenCalledTimes(1);
+      expect(onSubmit.mock.calls[0][0]).toMatchObject({
+        new_password: 'test',
+        verify_password: 'test',
+      });
+
+      /*
+      expect(onSubmit).toHaveBeenCalledWith(
+        {
+          email: 'test@gmail.com',
+          password: 'test',
+        }
+      );
+      */
     });
   });
 });
