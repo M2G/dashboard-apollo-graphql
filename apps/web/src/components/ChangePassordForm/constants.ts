4f50b3660f1c66a7f2d8945196bc799d2de6a6ac
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-11 21:26:23 +0100

    feat: update constants

--- apps/web/src/components/ChangePassordForm/constants.ts
+++ apps/web/src/components/ChangePassordForm/constants.ts
@@ -1,43 +1,54 @@
 import { z } from 'zod';
 
 export const INPUT_NAME = {
-  OLD_PASSWORD: 'old_password',
+  OLD_PASSWORD: 'oldPassword',
   PASSWORD: 'password',
-  PASSWORD2: 'password_again',
+  CONFIRM_PASSWORD: 'confirmPassword',
 };
 
 export const INITIAL_VALUES = {
   [INPUT_NAME.OLD_PASSWORD]: '',
   [INPUT_NAME.PASSWORD]: '',
-  [INPUT_NAME.PASSWORD2]: '',
+  [INPUT_NAME.CONFIRM_PASSWORD]: '',
 };
 
 export const ERROR_TEXT_REQUIRED = {
-  ERROR_TEXT_REQUIRED_MATCH: 'Mot de passe 1 doit être égal au Mot de passe 2',
+  ERROR_TEXT_MATCH: 'Passwords does not match',
   ERROR_TEXT_REQUIRED_OLD_PASSWORD: 'Mot de passe requis',
   ERROR_TEXT_REQUIRED_PASSWORD: 'Mot de passe requis',
-  ERROR_TEXT_REQUIRED_PASSWORD2: 'Mot de passe 2 requis',
+  ERROR_TEXT_REQUIRED_PASSWORD2: 'Mot de passe confirme requis',
+  ERROR_TEXT_REQUIRED_PASSWORD_LENGTH:
+    'Mot de passe doit etre au moins 6 caracteres',
+  // Password must be at least 6 characters
 };
 
 export const LABEL_OLD_PASSWORD = 'Old Password';
 export const LABEL_PASSWORD = 'Password';
-export const LABEL_PASSWORD2 = 'Password';
+export const LABEL_CONFIRM_PASSWORD = 'Password';
 
 export const PLACEHOLDER_OLD_PASSWORD = 'Old Mot de passe';
 export const PLACEHOLDER_PASSWORD = 'Mot de passe';
-export const PLACEHOLDER_PASSWORD2 = 'Mot de passe 2';
+export const PLACEHOLDER_PASSWORD2 = 'Mot de passe confirme';
 
 export const formSchema = z.object({
   [INPUT_NAME.OLD_PASSWORD]: z
     .string()
-    .min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_OLD_PASSWORD),
+    .min(6, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_OLD_PASSWORD),
   [INPUT_NAME.PASSWORD]: z
     .string()
-    .min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_PASSWORD)
-    .refine((data) => data[INPUT_NAME.PASSWORD] === data[INPUT_NAME.PASSWORD2], {
-      message: ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_MATCH,
-      path: [INPUT_NAME.PASSWORD],
+    .min(6, {
+      message: ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_PASSWORD_LENGTH,
+    })
+    .refine(
+      (data) => data[INPUT_NAME.PASSWORD] === data[INPUT_NAME.CONFIRM_PASSWORD],
+      {
+        message: ERROR_TEXT_REQUIRED.ERROR_TEXT_MATCH,
+        path: [INPUT_NAME.PASSWORD],
+      },
+    ),
+  [INPUT_NAME.CONFIRM_PASSWORD]: z
+    .string()
+    .min(6, {
+      message: ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_PASSWORD_LENGTH,
     }),
-  [INPUT_NAME.PASSWORD2]: z.string().min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_PASSWORD2),
-  // .min(8, 'Password must have more than 8 characters'),
 });
