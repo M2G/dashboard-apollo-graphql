b60dd3a3f36a67c9c47479de1403b2c49847f942
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-19 03:33:12 +0100

    feat: update reset pwd

--- apps/web/src/containers/ResetPassword/ResetPassword.tsx
+++ apps/web/src/containers/ResetPassword/ResetPassword.tsx
@@ -1,37 +1,41 @@
 /*eslint-disable*/
 import { useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useResetPasswordMutation } from 'modules/graphql/generated';
+import { useResetPasswordMutation } from '@/modules/graphql/generated';
 import { INITIAL_VALUES } from './constants';
-import ResetPasswordView from './ResetPasswordView';
-import ResetPasswordStatus from '../ResetPassword/ResetPasswordStatus';
+import ResetPasswordForm from '@/components/ResetPasswordForm';
+import ResetPasswordStatus from './ResetPasswordStatus';
 
 function ResetPassword(): JSX.Element | null {
   let { search } = useLocation();
   const searchParams = new URLSearchParams(search);
-  const [resetPasswordMutation, { loading, error, data }] = useResetPasswordMutation();
+  const [resetPasswordMutation, { loading, error, data }] =
+    useResetPasswordMutation();
 
   const success: boolean | null | undefined = data?.resetPassword?.success;
 
   if (!searchParams.has('token')) return null;
   const token = searchParams.get('token') as string;
 
-  const onSubmit = useCallback(async ({ new_password  }: { readonly new_password: string; }) => {
-    await resetPasswordMutation({
-      variables: {
-        input: {
-          password: new_password,
-          token
-        }
-      },
-    })
-  }, []);
+  const onSubmit = useCallback(
+    async ({ new_password }: { readonly new_password: string }) => {
+      await resetPasswordMutation({
+        variables: {
+          input: {
+            password: new_password,
+            token,
+          },
+        },
+      });
+    },
+    [],
+  );
 
   if (success && !loading) return <ResetPasswordStatus />;
-  if (error) return <>{JSON.stringify(error?.message)}</>
+  if (error) return <>{JSON.stringify(error?.message)}</>;
 
   return (
-    <ResetPasswordView initialValues={INITIAL_VALUES} onSubmit={onSubmit} />
+    <ResetPasswordForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />
   );
 }
 
