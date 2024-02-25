634257a3879fbc8039be5ef1c47d71a1d054a2d1
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-02-25 23:32:10 +0100

    feat: code refactor reset pwd form

--- apps/web/src/components/ResetPasswordForm/ResetPasswordForm.tsx
+++ apps/web/src/components/ResetPasswordForm/ResetPasswordForm.tsx
@@ -6,12 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
 import { Button, Field } from 'ui';
-import {
-  formSchema,
-  INPUT_NAME,
-  LABEL_NEW_PASSWORD,
-  LABEL_VERIFY_PASSWORD,
-} from './constants';
+import { formSchema, INPUT_NAME } from './constants';
 import { Link } from 'react-router-dom';
 import ROUTER_PATH from '@/constants/RouterPath';
 import { useMemo } from 'react';
