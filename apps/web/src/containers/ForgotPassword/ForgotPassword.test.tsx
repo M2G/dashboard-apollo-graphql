e77aefd72dfddff9b038ec97cf6803b58a4404b6
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-03-04 03:35:45 +0100

    feat: add forgot pwd test

--- apps/web/src/containers/ForgotPassword/ForgotPassword.test.tsx
+++ apps/web/src/containers/ForgotPassword/ForgotPassword.test.tsx
@@ -1,45 +1,141 @@
 /* eslint-disable */
-import { fireEvent, render, cleanup } from '@testing-library/react';
-import ForgotPasswordView from './ForgotPasswordView';
-import { INPUT_NAME } from './constants';
+import {
+  fireEvent,
+  render,
+  cleanup,
+  screen,
+  act,
+} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { MockedProvider } from '@apollo/client/testing';
+import ForgotPassword from './ForgotPassword';
+import {
+  ForgotPasswordDocument,
+  ResetPasswordDocument,
+} from '@/modules/graphql/generated';
+import { GraphQLError } from 'graphql';
+import ResetPassword from '@/containers/ResetPassword';
 
-afterEach(cleanup);
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest
+    .spyOn(URLSearchParams.prototype, 'get')
+    .mockReturnValue('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+});
 
-describe('Signup Container', () => {
-  describe('Submitting form', () => {
+describe('Forgot password Container', () => {
+  describe('Forgot password success', () => {
     let wrapper: any;
-    let floatingInput: HTMLInputElement;
-    let floatingPassword: HTMLInputElement;
-    const onSubmit = jest.fn();
+    let inputEmail: HTMLInputElement;
+    let btnSubmit: HTMLButtonElement;
 
+    afterEach(cleanup);
     beforeEach(() => {
-      const INITIAL_VALUES = {
-        [INPUT_NAME.EMAIL]: ''
-      };
+      const mocks = [
+        {
+          //delay: 30,
+          request: {
+            query: ForgotPasswordDocument,
+            variables: {
+              email: 'test@gmail.com',
+            },
+          },
+          result: {
+            data: {
+              forgotPassword: {
+                __typename: 'Status',
+                success: true,
+              },
+            },
+          },
+        },
+      ];
 
-      wrapper = render(
-        <ForgotPasswordView initialValues={INITIAL_VALUES} onSubmit={onSubmit} />
+      render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <MemoryRouter initialEntries={['/forgot-password']}>
+            <ForgotPassword />
+          </MemoryRouter>
+        </MockedProvider>,
       );
 
-      floatingInput = wrapper.container.querySelector('#floatingInput');
-      floatingPassword = wrapper.container.querySelector('#floatingPassword');
-
-      fireEvent.change(floatingInput, { target: { value: 'test@gmail.com' } });
-      fireEvent.change(floatingPassword, { target: { value: 'test' } });
-
-      expect(floatingInput).toBeInTheDocument();
-      expect(floatingPassword).toBeInTheDocument();
-
-      const button: any = wrapper.container.querySelector('.btn');
-      fireEvent.click(button);
+      inputEmail = screen.getByTestId('email');
+      btnSubmit = screen.getByTestId('submit');
     });
 
-    test('should render', () => {
-      expect(onSubmit).toHaveBeenCalledTimes(1);
-      expect(onSubmit).toHaveBeenCalledWith({
-        email: 'test@gmail.com',
-        password: 'test'
+    it('should success forgot password', async () => {
+      fireEvent.change(inputEmail, { target: { value: 'test@gmail.com' } });
+
+      await act(() => {
+        fireEvent.submit(btnSubmit);
       });
+
+      expect(
+        await screen.findByText('Your request has been processed successfully'),
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe('Submitting form (2)', () => {
+    let inputNewPassword: HTMLInputElement;
+    let inputVerifyPassword: HTMLInputElement;
+    let btnSubmit: HTMLButtonElement;
+
+    afterEach(cleanup);
+    beforeEach(() => {
+      const mocks = [
+        {
+          //delay: 30,
+          request: {
+            query: ForgotPasswordDocument,
+            variables: {
+              email: 'test@gmail.com',
+            },
+          },
+          result: {
+            data: {
+              forgotPassword: {
+                __typename: 'Status',
+                success: false,
+              },
+            },
+          },
+        },
+      ];
+
+      render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <MemoryRouter initialEntries={['/reset-password']}>
+            <ResetPassword />
+          </MemoryRouter>
+        </MockedProvider>,
+      );
+
+      inputNewPassword = screen.getByTestId('new_password');
+      inputVerifyPassword = screen.getByTestId('verify_password');
+      btnSubmit = screen.getByTestId('submit');
+    });
+
+    it('should fail reset password', async () => {
+      fireEvent.change(inputNewPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71' },
+      });
+      fireEvent.change(inputVerifyPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71' },
+      });
+
+      await act(() => {
+        fireEvent.submit(btnSubmit);
+      });
+
+      expect(
+        await screen.findByText(
+          'An error occured while resetting your password',
+        ),
+      ).toBeInTheDocument();
     });
   });
 });
+
+// An error has occurred during your request
