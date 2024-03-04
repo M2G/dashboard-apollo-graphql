0eda43c7439d9139137530f19f957bd94711ef9f
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-03-04 03:36:26 +0100

    feat: code refactor forgot pwd status

--- apps/web/src/containers/ForgotPassword/ForgotPasswordStatus.tsx
+++ apps/web/src/containers/ForgotPassword/ForgotPasswordStatus.tsx
@@ -1,5 +1,30 @@
-function ForgotPasswordStatus(): JSX.Element {
-  return <div>Message sent successfully</div>;
+import { useTranslation } from 'react-i18next';
+
+interface IForgotPasswordStatus {
+  success?: boolean;
+}
+
+function ForgotPasswordStatus({
+  success = false,
+}: IForgotPasswordStatus): JSX.Element {
+  const { t } = useTranslation();
+  return (
+    <div
+      className="flex min-h-screen flex-col items-center justify-center"
+      id="form-forgot-password">
+      <div>
+        {success
+          ? t(
+              'forgotPassword.forgotPasswordConfirmation',
+              'Your request has been processed successfully',
+            )
+          : t(
+              'forgotPassword.forgotPasswordError',
+              'An error has occurred during your request',
+            )}
+      </div>
+    </div>
+  );
 }
 
 export default ForgotPasswordStatus;
