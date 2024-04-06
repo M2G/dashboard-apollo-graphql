f245a8132a291657fc6e2273255de294a29fae74
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-04-06 04:19:37 +0200

    feat: code refactor

--- apps/web/src/components/NoData/NoData.tsx
+++ apps/web/src/components/NoData/NoData.tsx
@@ -1,5 +1,8 @@
+import { useTranslation } from 'react-i18next';
+
 function NoData(): JSX.Element {
-  return <div>No data</div>;
+  const { t } = useTranslation();
+  return <div>{t('noData', 'No Data')}</div>;
 }
 
 export default NoData;
