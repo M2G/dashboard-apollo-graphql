b5b0ce7c36f8ad7e0ee0987932329307e7c6415e
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-04-06 04:21:57 +0200

    feat: add test

--- apps/web/src/containers/Profil/Profil.test.tsx
+++ apps/web/src/containers/Profil/Profil.test.tsx
@@ -1,51 +1,164 @@
-import { useCallback } from 'react';
-import { useAuth } from '@/AuthContext';
-import ChangePasswordForm from '@/components/ChangePassordForm';
-import { INITIAL_VALUES } from '@/components/ChangePassordForm/constants';
-import Loading from '@/components/Loading';
-import { useChangePasswordMutation } from '@/modules/graphql/generated';
-import { useTranslation } from 'react-i18next';
-import { toast } from 'react-toastify';
+/* eslint-disable */
+import {
+  fireEvent,
+  render,
+  cleanup,
+  screen,
+  act,
+} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
+import ChangePassword from './ChangePassword';
+import { ChangePasswordDocument } from '@/modules/graphql/generated';
+import AuthContext from '@/AuthContext';
+import { GraphQLError } from 'graphql';
 
-function ChangePassword(): JSX.Element {
-  const {
-    userData: { id },
-  } = useAuth();
-  const [changePasswordMutation, { data, error, loading }] =
-    useChangePasswordMutation();
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
-  console.log('useAuth useAuth useAuth', id);
+describe('Change password Container', () => {
+  describe('Change password success', () => {
+    let inputOldPassword: HTMLInputElement;
+    let inputPassword: HTMLInputElement;
+    let inputConfirmPassword: HTMLInputElement;
+    let btnSubmit: HTMLButtonElement;
 
-  const handleSubmit = useCallback(
-    (formData: { id: string; oldPassword: string; password: string }) => {
-      changePasswordMutation({
-        variables: {
-          id,
-          input: {
-            oldPassword: formData.oldPassword,
-            password: formData.password,
+    afterEach(cleanup);
+    beforeEach(() => {
+      const mocks = [
+        {
+          //delay: 30,
+          request: {
+            query: ChangePasswordDocument,
+            variables: {
+              id: 1,
+              input: {
+                oldPassword: '9Ij!Z-Tb)nft73OpLpw£71----',
+                password: '9Ij!Z-Tb)nft73OpLpw£71',
+              },
+            },
+          },
+          result: {
+            data: {
+              changePassword: {
+                __typename: 'Status',
+                success: true,
+              },
+            },
           },
         },
+      ];
+
+      render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <AuthContext.Provider>
+            <MemoryRouter initialEntries={['/change-password']}>
+              <ChangePassword />
+            </MemoryRouter>
+          </AuthContext.Provider>
+        </MockedProvider>,
+      );
+
+      inputOldPassword = screen.getByTestId('oldPassword');
+      inputPassword = screen.getByTestId('password');
+      inputConfirmPassword = screen.getByTestId('confirmPassword');
+      btnSubmit = screen.getByTestId('submit');
+    });
+
+    it('should success change password', async () => {
+      fireEvent.change(inputOldPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71----' },
       });
-    },
-    [changePasswordMutation, id],
-  );
+      fireEvent.change(inputPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71' },
+      });
+      fireEvent.change(inputConfirmPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71' },
+      });
 
-  if (loading) return <Loading isLoading={loading} />;
+      await act(() => {
+        fireEvent.submit(btnSubmit);
+      });
 
-  const success = data?.changePassword?.success;
-  const messageError = error?.networkError?.result?.errors?.[0]?.message;
+      screen.debug();
 
-  console.log('success success success', success);
+      expect(
+        await screen.findByText('Your password has been changed successfully'),
+      ).toBeInTheDocument();
+    });
+  });
+  describe('Change password fail', () => {
+    let inputOldPassword: HTMLInputElement;
+    let inputPassword: HTMLInputElement;
+    let inputConfirmPassword: HTMLInputElement;
+    let btnSubmit: HTMLButtonElement;
 
-  return (
-    <ChangePasswordForm
-      error={messageError}
-      initialValues={INITIAL_VALUES}
-      onSubmit={handleSubmit}
-      success={success}
-    />
-  );
-}
+    afterEach(cleanup);
+    beforeEach(() => {
+      const mocks = [
+        {
+          //delay: 30,
+          request: {
+            query: ChangePasswordDocument,
+            variables: {
+              id: 1,
+              input: {
+                oldPassword: '9Ij!Z-Tb)nft73OpLpw£71----',
+                password: '9Ij!Z-Tb)nft73OpLpw£71',
+              },
+            },
+          },
+          result: {
+            errors: [new GraphQLError('Passwords does not match')],
+            data: {
+              changePassword: {
+                __typename: 'Status',
+                success: false,
+              },
+            },
+          },
+        },
+      ];
 
-export default ChangePassword;
+      render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <AuthContext.Provider>
+            <MemoryRouter initialEntries={['/change-password']}>
+              <ChangePassword />
+            </MemoryRouter>
+          </AuthContext.Provider>
+        </MockedProvider>,
+      );
+
+      inputOldPassword = screen.getByTestId('oldPassword');
+      inputPassword = screen.getByTestId('password');
+      inputConfirmPassword = screen.getByTestId('confirmPassword');
+      btnSubmit = screen.getByTestId('submit');
+    });
+
+    it('should success forgot password', async () => {
+      fireEvent.change(inputOldPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71----' },
+      });
+      fireEvent.change(inputPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71' },
+      });
+      fireEvent.change(inputConfirmPassword, {
+        target: { value: '9Ij!Z-Tb)nft73OpLpw£71' },
+      });
+
+      await act(() => {
+        fireEvent.submit(btnSubmit);
+      });
+
+      screen.debug();
+
+      expect(
+        await screen.findByText('Passwords does not match'),
+      ).toBeInTheDocument();
+    });
+  });
+});
+
+// An error has occurred during your request
