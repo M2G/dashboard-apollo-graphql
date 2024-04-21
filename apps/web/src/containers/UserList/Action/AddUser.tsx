c4c5c82ea657d91ddb25c4c111022034cda75558
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-04-21 03:20:43 +0200

    feat: code refactor

--- apps/web/src/containers/UserList/Action/AddUser.tsx
+++ apps/web/src/containers/UserList/Action/AddUser.tsx
@@ -1,19 +1,16 @@
 import { useTranslation } from 'react-i18next';
 
 interface IAddUser {
-  canAdd: boolean;
   onAdd: () => void;
 }
 
-function AddUser({ canAdd, onAdd }: IAddUser) {
+function AddUser({ onAdd }: IAddUser) {
   const { t } = useTranslation();
   return (
     <div className="c-btn-user">
-      {canAdd && (
-        <button className="btn btn-light" type="submit" onClick={onAdd}>
-          {t('Add user')}
-        </button>
-      )}
+      <button className="btn btn-light" onClick={onAdd} type="submit">
+        {t('Add user')}
+      </button>
     </div>
   );
 }
